f88bc8c8f2fc8a74476c0398c3ab1aef2c0820c7
STEPS0N <stepan.klimoff@mail.ru>
2026-01-07 13:37:53 +0500

    Разработанные интерфейсы. Климов

---
+++ Shop_Климов/Interfaces/IContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Климов.Interfaces
+{
+    public interface IContext
+    {
+        List<object> All();
+
+        void Save(bool Update = false);
+
+        void Delete();
+    }
+}
