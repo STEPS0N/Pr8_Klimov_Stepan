c3f71411c0b73c39065ea4a9801acb687a828b7d
STEPS0N <stepan.klimoff@mail.ru>
2026-01-07 14:24:08 +0500

    Разработанный контекст для детских товаров. Климов

---
+++ Shop_Климов/Classes/ChildrenContext.cs
@@ -0,0 +1,51 @@
+﻿using Shop_Климов.Interfaces;
+using Shop_Климов.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Климов.Classes
+{
+    public class ChildrenContext : Children, IContext
+    {
+        public ChildrenContext() { }
+        public ChildrenContext(int Id, string Name, int Price, int Age, int IdShop) { }
+        public List<object> All()
+        {
+            List<object> allShop = new ShopContext().All();
+            List<object> allChildren = new List<object>();
+            OleDbConnection connection = Common.DBConnection.Connection();
+            OleDbDataReader childrenData = Common.DBConnection.Query("SELECT * FROM [Детские вещи]", connection);
+            while (childrenData.Read())
+            {
+                ShopContext shopElement = allShop.Find(
+                    x => (x as ShopContext).Id == childrenData.GetInt32(2)) as ShopContext;
+                ChildrenContext newChildren = new ChildrenContext(
+                    shopElement.Id,
+                    shopElement.Name,
+                    shopElement.Price,
+                    childrenData.GetInt32(1),
+                    childrenData.GetInt32(2)
+                    );
+                allChildren.Add(newChildren);
+            }
+
+            Common.DBConnection.CloseConnection(connection);
+
+            return allChildren;
+        }
+
+        public void Save (bool Update = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Shop_Климов/Models/Children.cs
+++ Shop_Климов/Models/Children.cs
@@ -9,9 +9,12 @@ namespace Shop_Климов.Models
     public class Children : Shop
     {
         public int Age { get; set; }
-        public Children(string Name, int Price, string Image, int Age) : base(Name, Price, Image)
+        public int IdShop { get; set; }
+        public Children() { }
+        public Children(int Id, string Name, int Price, int Age, int IdShop) : base(Id, Name, Price)
         {
             this.Age = Age;
+            this.IdShop = IdShop;
         }
     }
 }
