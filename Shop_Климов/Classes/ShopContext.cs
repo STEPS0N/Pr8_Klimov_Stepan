0f3ae3693f1fd1364e79c9941c5188a289bb540f
STEPS0N <stepan.klimoff@mail.ru>
2026-01-07 14:08:36 +0500

    Разработанный контекст для товара. Климов

---
+++ Shop_Климов/Classes/ShopContext.cs
@@ -0,0 +1,48 @@
+﻿using Shop_Климов.Interfaces;
+using Shop_Климов.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Климов.Classes
+{
+    public class ShopContext : Shop, IContext
+    {
+        public ShopContext() { }
+
+        public ShopContext(int Id, string Name, int Price) : base(Id, Name, Price) { }
+
+        public List<object> All()
+        {
+            List<object> allShop = new List<object>();
+
+            OleDbConnection connection = Common.DBConnection.Connection();
+            OleDbDataReader shopData = Common.DBConnection.Query("SELECT * FROM [Товар]", connection);
+            while (shopData.Read())
+            {
+                ShopContext newShop = new ShopContext(
+                    shopData.GetInt32(0),
+                    shopData.GetString(1),
+                    shopData.GetInt32(2)
+                    );
+                allShop.Add(newShop);
+            }
+            Common.DBConnection.CloseConnection(connection);
+
+            return allShop;
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(bool Update = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Shop_Климов/Models/Shop.cs
+++ Shop_Климов/Models/Shop.cs
@@ -9,14 +9,18 @@ namespace Shop_Климов.Models
 {
     public class Shop
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public string Image { get; set; } = "/Images/ic_item.png";
-        public Shop(string Name, int Price, string Image)
+
+        public Shop() { }
+
+        public Shop(int Id, string Name, int Price)
         {
+            this.Id = Id;
             this.Name = Name;
             this.Price = Price;
-            this.Image = Image;
         }
     }
 }
